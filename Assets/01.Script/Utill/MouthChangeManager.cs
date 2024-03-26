7400bbf147b44a3da399be4758d20376e13972fe
StraightBet <slupgames1025@gmail.com>
2024-03-26 19:18:44 +0900

    0326_발음 기호에 따른 입모양 타입 받아오기 개발

--- Assets/01.Script/Utill/MouthChangeManager.cs
+++ Assets/01.Script/Utill/MouthChangeManager.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class MouthChangeManager : MonoBehaviour
 {
-    private string ipaString = "";
-    // Start is called before the first frame update
+    public string ipaString = "";
+
+    public char[] vowelIPAList = new char[29];
+    public int [] vowelMouthTypeList = new int [29];
+    /* index 에 따른 입 모양
+    0 : idle
+    1 : ㅏ
+    2 : ㅓ
+    3 : ㅗ
+    4 : ㅜ
+    5 : ㅡ
+    6 : ㅣ
+    7 : ㅔ
+    8 : ㅐ
+    */
+    public bool[] nowMouthShape = new bool[9];
+    public bool[] nowMouthType = new bool[11];
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -17,7 +32,42 @@ public class MouthChangeManager : MonoBehaviour
         
     }
 
-    private void ChangeMouthTo()
+    public IEnumerator IPAtoMouthChange()
+    {
+        WaitForSeconds _newtWaitTime = new WaitForSeconds(0.125f);
+        string _ipaString = ipaString;
+
+        string _checkMessage = "";
+
+        for(int i = 0; i < _ipaString.Length; i++)
+        {
+            _checkMessage += IPAtoMouthType(_ipaString[i]) + " ";
+        }
+        Debug.Log(_checkMessage);
+        return null;
+    }
+
+    public void Check()
+    {
+        StartCoroutine("IPAtoMouthChange");
+    }
+
+    private int IPAtoMouthType(char _nowIPA)
+    {
+        int _mouthType = 0;
+        for(int i = 0; i < vowelIPAList.Length; i++)
+        {
+            if(vowelIPAList[i] == _nowIPA)
+            {
+                _mouthType = vowelMouthTypeList[i];
+                break;
+            }
+        }
+
+        return _mouthType;
+    }
+
+    private void MouthChange_Beta()
     {
 
     }
